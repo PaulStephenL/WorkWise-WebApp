78fd2cbf122b298ba8445bd583e57701297df65e
kuldeep56789 <kuldeepjadeja257@gmail.com>
2025-03-17 12:00:00 +0530

    Imports : Added Imports for company pages in AdminDashboard

--- src/pages/Admin/Dashboard/AdminDashboard.jsx
+++ src/pages/Admin/Dashboard/AdminDashboard.jsx
@@ -4,6 +4,8 @@ import { supabase, checkAndVerifyAdminRole ***REMOVED*** from '../../../lib/supa
 import { Briefcase, Building2, Users, LayoutDashboard, MapPin ***REMOVED*** from 'lucide-react';
 import JobsList from '../Jobs/JobsList';
 import JobDetails from '../Jobs/JobDetails';
+import CompaniesList from '../Companies/CompaniesList';
+import CompanyDetails from '../Companies/CompanyDetails';
 import ApplicationsList from '../Applications/ApplicationsList';
 import ApplicationDetails from '../Applications/ApplicationDetails';
 import { toast ***REMOVED*** from 'react-hot-toast';
