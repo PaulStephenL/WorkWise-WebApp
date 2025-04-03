53bf3b4ed0e996ba032ad426ee0dbe0520be45be
Paul Stephen Lawrence <paulslawrence04@gmail.com>
2025-04-03 12:43:26 +0530

    Modularization - 2 - Auth buttons 'Login' and 'SignUp' seperated from navbar

---
+++ src/components/Navbar/AuthButtons.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link ***REMOVED*** from 'react-router-dom';
+
+function AuthButtons() {
+  return (
+    <>
+      <Link
+        to="/login"
+        className="px-4 py-2 rounded bg-[#798478] hover:bg-[#a0a083] transition-colors"
+      >
+        Login
+      </Link>
+      <Link
+        to="/signup"
+        className="px-4 py-2 rounded bg-[#ffdde2] text-[#101d42] hover:bg-opacity-90 transition-colors"
+      >
+        Sign Up
+      </Link>
+    </>
+  );
+***REMOVED***
+
+export default AuthButtons; 
